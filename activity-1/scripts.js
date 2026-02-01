74ead719998185180a537c3426c3d2a4b5151c63
shultza <shultza@matc.edu>
2026-02-01 12:27:15 -0600

    Complete Activity 1
    i hate git

---
+++ activity-1/scripts.js
@@ -0,0 +1,28 @@
+//Part A
+console.log("Hello, World!");
+//Part B
+document.getElementById("output").innerText = "Hello, World!";
+//Part C
+let studentName = "Alexander Shultz";
+const age = 19;
+let isStudent = true;
+let emptyValue = null;
+let notAssigned;
+//Part D
+console.log("Values of variables");
+console.log("Student Name:", studentName);
+console.log("Age:", age);
+console.log("Is a Student?:", isStudent);
+console.log("Empty Value:", emptyValue);
+console.log("Not Assigned:", notAssigned);
+
+console.log("Variable Types");
+console.log("Type of studentName:", typeof studentName);
+console.log("Type of age:", typeof age);
+console.log("Type of isStudent:", typeof isStudent);
+console.log("Type of emptyValue:", typeof emptyValue);
+console.log("Type of notAssigned:", typeof notAssigned);
+//Part E
+console.log("OG Name: ", studentName);
+studentName = "Mickey Mouse";
+console.log("New name: ", studentName);
